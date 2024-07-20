2ebe476ab83c1b4f69018b0ec02817cc4dfc32d5
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-07-20 20:27:18 +0300

    Update contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,29 +1,37 @@
 import createHttpError from "http-errors";
 import { getAllContacts, getContactById } from "../services/contacts.js";
 
-async function getAllContactsController(req, res) {
-  const contacts = getAllContacts();
+async function getAllContactsController(req, res, next) {
+  try {
+    const contacts = await getAllContacts();
 
-  res.status(200).json({
-    status: 200,
-    message: "Successfully found contacts!",
-    data: contacts,
-  });
-}
-
-async function getContactByIdController(req, res) {
-  const { contact_id } = req.params;
-  const contact = await getContactById(contact_id);
-
-  if (!contact) {
-    res.createHttpError(404, "Contact not found with id ");
-    return;
+    res.status(200).json({
+      status: 200,
+      message: "Successfully found contacts!",
+      data: contacts,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  res.status(200).json({
-    status: 200,
-    message: `Successfully found contact with id ${contact_id}!`,
-    data: contact,
-  });
 }
+
+async function getContactByIdController(req, res, next) {
+  try {
+    const { contact_id } = req.params;
+    const contact = await getContactById(contact_id);
+
+    if (!contact) {
+      throw createHttpError(404, `Contact not found with id ${contact_id}`);
+    }
+
+    res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contact_id}!`,
+      data: contact,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
 export { getAllContactsController, getContactByIdController };
