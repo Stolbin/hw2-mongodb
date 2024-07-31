931e1bd076b1be69532818720e4130e2cb4e2539
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-07-31 14:35:48 +0300

    push task

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,10 +6,22 @@ import {
   updateContact,
 } from "../services/contacts.js";
 import createHttpError from "http-errors";
+import { parsePaginationParams } from "../utils/parsePaginationParams.js";
+import { parseSortParams } from "../utils/parseSortParams.js";
+import { parseFilterParams } from "../utils/parseFilterParams.js";
 
 export async function getAllContactsController(req, res, next) {
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
   try {
-    const contacts = await getAllContacts();
+    const contacts = await getAllContacts({
+      page,
+      perPage,
+      sortBy,
+      sortOrder,
+      filter,
+    });
 
     res.json({
       status: 200,
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,47 @@
 import { Contact } from "../db/models/contact.js";
+import { calculatePaginationData } from "../utils/calculatePaginationData.js";
+import { SORT_ORDER } from "../constants/constans.js";
 
-export const getAllContacts = async () => {
-  const contacts = await Contact.find();
-  return contacts;
+// export const getAllContacts = async () => {
+//   const contacts = await Contact.find();
+//   return contacts;
+// };
+
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = "_id",
+  filter = {},
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
+
+  const contactsQuery = Contact.find();
+
+  if (filter.contactType) {
+    contactsQuery.where("contactType").equals(filter.contactType);
+  }
+
+  if (filter.isFavourite) {
+    contactsQuery.where("isFavourite").equals(filter.isFavourite);
+  }
+
+  const [contacts, contactsCount] = await Promise.all([
+    Contact.countDocuments(),
+    Contact.find()
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
 
 export const getContactById = async (contactId) => {
@@ -29,10 +68,8 @@ export const updateContact = async (contactId, payload, options = {}) => {
     { new: true, includeResultMetadata: true, ...options }
   );
 
-  if (!rawResult || !rawResult.value) return null;
-
-  return {
-    contact: rawResult,
-    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
-  };
+  if (!rawResult || !rawResult.value) {
+    return null;
+  }
+  return rawResult.value;
 };
