992fbf17bfb271da50dc99854435704f651677ff
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-08-01 19:15:13 +0300

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,17 +1,18 @@
 import Joi from "joi";
+import { EMAIL_REGEX } from "../constants/constans.js";
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(25).required(),
-  phoneNumber: Joi.string().required(),
-  email: Joi.string().min(5).max(30).email().required(),
+  phoneNumber: Joi.string().min(5).max(25).required(),
+  email: Joi.string().min(5).max(25).pattern(EMAIL_REGEX).required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid("work", "home", "personal"),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(25).required(),
-  phoneNumber: Joi.string().required(),
-  email: Joi.string().min(5).max(30).email().required(),
+  phoneNumber: Joi.string().min(5).max(25),
+  email: Joi.string().min(5).max(25).pattern(EMAIL_REGEX),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid("work", "home", "personal"),
 });
