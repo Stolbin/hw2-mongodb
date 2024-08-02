ff0f4463f12c4f45f4fd4153a1d6ca9f39d478d2
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-08-02 09:59:25 +0300

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -35,8 +35,8 @@ export const getAllContacts = async ({
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
 
   return {
-    ...paginationData,
     data: contacts,
+    ...paginationData,
   };
 };
 
