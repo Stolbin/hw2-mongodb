4771c1e2e0ee79d78ede50bc546de8da0ed5fdb1
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-08-19 13:40:36 +0300

    upd

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 export const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
+  console.error(err);
   res.status(err.status || 500).json({
     status: err.status || 500,
     message: err.message || "Internal Server Error",
