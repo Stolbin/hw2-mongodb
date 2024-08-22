66635cfcef8e2c7f9eadb567f032813360783068
Stolbin <109080579+Stolbin@users.noreply.github.com>
2024-08-22 14:30:20 +0300

    upd

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import router from "./routers/index.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFoundErrorHandler } from "./middlewares/notFoundHandler.js";
 import cookieParser from "cookie-parser";
+import { swaggerDocs } from "./middlewares/swaggerDocs.js";
 import { UPLOAD_DIR } from "./constants/constans.js";
 
 dotenv.config();
@@ -13,12 +14,16 @@ dotenv.config();
 export const setupServer = () => {
   const app = express();
   const PORT = Number(process.env.PORT || 3000);
+
+  app.use(router);
+
   app.use(
     express.json({
       type: ["application/json", "application/vnd.api+json"],
       limit: "100kb",
     })
   );
+
   app.use(cookieParser());
 
   app.use(cors());
@@ -32,7 +37,8 @@ export const setupServer = () => {
   );
 
   app.use("/uploads", express.static(UPLOAD_DIR));
-  app.use(router);
+
+  app.use("/api-docs", swaggerDocs());
 
   app.use("*", notFoundErrorHandler);
 
